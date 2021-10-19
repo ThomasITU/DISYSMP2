eab32988ce1a3bb364f7644568346ac595ee04c9
ThomasITU <thwr@itu.dk>
2021-10-19 10:02:08 +0200

    Rename ChittyChat

--- Server/server.go
+++ Server/server.go
@@ -1,9 +1,32 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	chat "github.com/lottejd/DISYSMP2/ChittyChat"
+	"google.golang.org/grpc"
+)
+
 const (
 	port = ":8080"
 )
 
 type Server struct {
-	ChittyChatServiceServer.UnimplementedChittChatServiceServer
+	chat.UnimplementedChittChatServiceServer
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	chat.RegisterChittChatServiceServer(grpcServer, &Server{})
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port %s  %v", port, err)
+	}
 }
