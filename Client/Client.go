82f4ae0b7ae77db39395efc68410099d2883366d
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-10-20 01:07:29 +0200

    add JoinChat, LeaveChat
    FormatVectorClock to fix formatError when printing
    
    Initial attemp at broadcasting

--- Client/Client.go
+++ Client/Client.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/lottejd/DISYSMP2/ChittyChat"
@@ -16,15 +19,17 @@ type bufferedMessage struct {
 }
 
 const (
-	address = "localhost:8080"
+	address       = "localhost:8080"
+	clientLogFile = "clientLogId_"
 )
 
-var clientId int
-
-var buffer chan (string)
+var (
+	clientId               int
+	lastestVectorTimeStamp []int32
+	buffer                 chan (bufferedMessage)
+)
 
 func main() {
-	buffer = make(chan string, 5)
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
@@ -32,12 +37,14 @@ func main() {
 	}
 	defer conn.Close()
 
+	// create client
 	chat := ChittyChat.NewChittyChatServiceClient(conn)
-
-	// Contact the server and print out its response.
 	ctx := context.Background()
+	JoinChat(ctx, chat)
 
-	//go GetBroadcast(buffer, chat)
+	buffer = make(chan bufferedMessage, 10)
+
+	go GetBroadcast(ctx, chat)
 
 	for {
 		var input string
@@ -47,21 +54,83 @@ func main() {
 	}
 }
 
-func GetBroadcast(buffer chan bufferedMessage, ctx context.Context, chat ChittyChat.ChittyChatServiceClient) {
+func GetBroadcast(ctx context.Context, chat ChittyChat.ChittyChatServiceClient) {
 
 	for {
-		time.Sleep(time.Second * 5)
-		//chat.GetBroadCast(ctx, )
+
+		time.Sleep(time.Second * 1)
+		//overvej at sende alle de seneste broadcasts, sorter lokalt ved clienten via lamport/vector clock
+
+		response, err := chat.GetBroadcast(ctx, &ChittyChat.GetBroadcastRequest{})
+		if err != nil {
+			Logger(err.Error(), clientLogFile+strconv.Itoa(clientId))
+			continue
+		}
+
+		vectorClockFromServer := response.GetClientsConnected()
+		broadCastIsNewer := false
+		for i := 0; i < len(vectorClockFromServer); i++ {
+			if vectorClockFromServer[i] < lastestVectorTimeStamp[i] {
+				broadCastIsNewer = true
+				lastestVectorTimeStamp[i] = vectorClockFromServer[i]
+			}
+		}
+
+		// print
+		log.Println(response.Msg + ", vectorClock: " + FormatVectorClock(lastestVectorTimeStamp))
+		Logger(response.Msg+", vectorClock: "+FormatVectorClock(lastestVectorTimeStamp), clientLogFile+strconv.Itoa(clientId))
+		if broadCastIsNewer {
+			log.Println(response.Msg + ", vectorClock: " + FormatVectorClock(lastestVectorTimeStamp))
+		}
+
 	}
 }
 
 func PublishFromClient(input string, ctx context.Context, chittyServer ChittyChat.ChittyChatServiceClient) {
+	inputFromClient := &ChittyChat.PublishRequest{Request: input, ClientId: int32(clientId)}
+	response, err := chittyServer.Publish(ctx, inputFromClient)
+	checkErr(err)
+	log.Println(response.GetMsg())
+}
 
-	inputFromClient := ChittyChat.PublishRequest{Request: input, ClientId: int32(clientId)}
+func JoinChat(ctx context.Context, chittyServer ChittyChat.ChittyChatServiceClient) {
+	response, err := chittyServer.JoinChat(ctx, &ChittyChat.JoinChatRequest{})
+	checkErr(err)
+	clientId = int(response.GetClientId())
+	log.Printf("connected with id: %v", clientId)
+}
 
-	response, err := chittyServer.Publish(ctx, &inputFromClient)
+func LeaveChat(ctx context.Context, chittyServer ChittyChat.ChittyChatServiceClient) {
+
+	response, err := chittyServer.LeaveChat(ctx, &ChittyChat.LeaveChatRequest{ClientId: int32(clientId)})
+	checkErr(err)
+	log.Printf(response.GetMsg())
+}
+
+//help methods
+func checkErr(err error) {
 	if err != nil {
-		log.Fatalf(err.Error())
+		log.Fatalln(err.Error())
 	}
-	log.Printf(response.GetName())
+}
+
+func FormatVectorClock(clock []int32) string {
+	var sb = strings.Builder{}
+	sb.WriteString("<")
+	for i := 0; i < len(clock); i++ {
+		sb.WriteString(strconv.Itoa(int(clock[i])))
+	}
+	sb.WriteString(">")
+	return sb.String()
+}
+
+func Logger(message string, logFileName string) {
+	f, err := os.OpenFile(logFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	log.Println(message)
 }
