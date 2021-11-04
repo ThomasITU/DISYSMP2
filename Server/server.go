6b34895f3509974079f4539c393a1615e02c0fd0
Thomas Wolgast Rørbech <thwr@itu.dk>
2021-11-04 19:31:26 +0100

    fix github import

---
+++ Server/server.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/ThomasITU/DISYSMP2/DISYSMP2/ChittyChat"
+)
+
+type chittyChatServer struct {
+	ChittyChat.UnimplementedChittyChatServiceServer
+}
+
+func main() {
+	
+}
